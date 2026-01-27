09789e4086f4435966e55bebb6b4341df326d1ba
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-01-27 19:48:02 +0000

    made max 50 bugs jitter

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 #include <bn_color.h>
 #include <bn_sprite_ptr.h>
 #include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
+
 #include "bn_sprite_items_bug.h"
+
 
 int main(){
     bn::core::init();
@@ -12,17 +16,26 @@ int main(){
 
     bn::backdrop::set_color(bn::color(15,15,0));
 
-    bn::sprite_ptr beetle = bn::sprite_items::bug.create_sprite();
+    bn::vector<bn::sprite_ptr, 50> beetles = {};
+
+    // bn::sprite_ptr beetle = bn::sprite_items::bug.create_sprite();
 
     while(true){
-        bn::fixed new_x = beetle.x() + rng.get_fixed(-1,1);
-        bn::fixed new_y = beetle.y() + rng.get_fixed(-1,1);
+        for(bn::sprite_ptr& beetle : beetles){
+            bn::fixed new_x = beetle.x() + rng.get_fixed(-1,1);
+            bn::fixed new_y = beetle.y() + rng.get_fixed(-1,1);
+            
+            beetle.set_x(new_x);
+            beetle.set_y(new_y);
+        }
 
-        beetle.set_x(new_x);
-        beetle.set_y(new_y);
+
+        if(bn::keypad::a_pressed()){
+            beetles.push_back(bn::sprite_items::bug.create_sprite());
+        }
 
 
-
+        rng.update();
         bn::core::update();
     }
 }
