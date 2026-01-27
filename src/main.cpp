54c2d716e99ad5a892ff79a20566e97fd536e582
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-01-27 19:25:14 +0000

    made bug jitter

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,27 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_sprite_ptr.h>
-
+#include <bn_random.h>
 #include "bn_sprite_items_bug.h"
 
 int main(){
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(31,0,31));
+    bn::random rng = bn::random();
+
+    bn::backdrop::set_color(bn::color(15,15,0));
 
     bn::sprite_ptr beetle = bn::sprite_items::bug.create_sprite();
 
     while(true){
+        bn::fixed new_x = beetle.x() + rng.get_fixed(-1,1);
+        bn::fixed new_y = beetle.y() + rng.get_fixed(-1,1);
+
+        beetle.set_x(new_x);
+        beetle.set_y(new_y);
+
+
+
         bn::core::update();
     }
 }
