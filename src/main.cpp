7144b9fac7fdef977462d7bc93fa66d0705d2f59
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-01-27 19:01:16 +0000

    added bug to screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+
+#include "bn_sprite_items_bug.h"
 
 int main(){
     bn::core::init();
 
     bn::backdrop::set_color(bn::color(31,0,31));
+
+    bn::sprite_ptr beetle = bn::sprite_items::bug.create_sprite();
 
     while(true){
         bn::core::update();
